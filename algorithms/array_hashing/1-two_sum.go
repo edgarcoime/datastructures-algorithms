87ca0bd705ea104cf11560fb9074c9c317707514
Edgar Coime <e.coimeii@gmail.com>
2023-04-20 14:29:53 -0700

    feat: 1-two sum - one pass O(n) soln

--- algorithms/array_hashing/1-two_sum.go
+++ algorithms/array_hashing/1-two_sum.go
@@ -12,15 +12,31 @@ func twoSum2(nums []int, target int) []int {
 		}
 	}
 
-	return []int{}
+	return nil
 }
 
+// Hash Map store complement
+// TC: O(n)
+// SC: O(n)
 func twoSum(nums []int, target int) []int {
-	return []int{}
+	// Create a map
+	// key (num) = value (index)
+	num_map := map[int]int{}
+
+	for i := 0; i < len(nums); i++ {
+		diff := target - nums[i]
+		if v, ok := num_map[diff]; ok {
+			return []int{i, v}
+		} else {
+			num_map[nums[i]] = i
+		}
+	}
+
+	return nil
 }
 
 func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
-	println(twoSum2(nums, target))
+	println(twoSum(nums, target))
 }
