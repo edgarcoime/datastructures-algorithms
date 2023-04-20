6477a7b861e222a259cdee3ebf0216f1d49fa1ea
Edgar Coime <e.coimeii@gmail.com>
2023-04-20 11:18:10 -0700

    feat: getters and setters

--- tutorial/1-basics/14-modules_packages/main.go
+++ tutorial/1-basics/14-modules_packages/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"reflect"
 
 	mypackage "example.com/project/mypackage"
@@ -13,4 +14,19 @@ func main() {
 	strArr := mypackage.IntArrToStrArr(intArr)
 	fmt.Println(strArr)
 	fmt.Println(reflect.TypeOf(strArr))
+
+	date := mypackage.Date{}
+	err := date.SetMonth(12)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(21)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetYear(1974)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("1st Day: %d/%d/%d\n", date.Year(), date.Month(), date.Day())
 }
---
+++ tutorial/1-basics/20-getter_setter.go
@@ -0,0 +1,11 @@
+package main
+
+// ----- PROTECTING DATA -----
+// We want to protect our data from receiving
+// bad values by moving our date struct
+// to another package using encapsulation
+// We'll use mypackage like before
+// ./14-modules_packages/mypackage/mypackage.go
+
+func main() {
+}
