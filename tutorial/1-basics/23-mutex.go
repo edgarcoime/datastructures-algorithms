78ed160b28bba93b142d67eef7c9f2fed10d61b2
Edgar Coime <e.coimeii@gmail.com>
2023-04-20 11:44:20 -0700

    feat: mutex

---
+++ tutorial/1-basics/23-mutex.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Account struct {
+	balance int
+	lock    sync.Mutex
+}
+
+func (a *Account) GetBalance() int {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.balance
+}
+
+func (a *Account) Withdraw(v int) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	if v > a.balance {
+		fmt.Println("Not enough money in account")
+	} else {
+		fmt.Printf("%d withdrawn | Balance: %d\n", v, a.balance)
+		a.balance -= v
+	}
+}
+
+func main() {
+	var acct Account
+	acct.balance = 100
+	println("Balance:", acct.GetBalance())
+	for i := 0; i < 12; i++ {
+		go acct.Withdraw(10)
+	}
+	time.Sleep(2 * time.Second)
+}
