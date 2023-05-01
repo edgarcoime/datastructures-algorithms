1d68c143f9328f90f6e680f31f494ab8c39022de
Edgar Coime <e.coimeii@gmail.com>
2023-05-01 10:20:36 -0700

    feat: 226 - invert binary tree

---
+++ algorithms/python-algorithms/trees/226-invert_binary_tree.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        left = self.invertTree(root.right)
+        root.right = self.invertTree(root.left)
+        root.left = left
+
+        return root
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
