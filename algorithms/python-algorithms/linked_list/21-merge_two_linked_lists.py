fb19d0659bdd35580ba2fc9f16f58f26ecca18d2
Edgar Coime <e.coimeii@gmail.com>
2023-05-04 11:31:13 -0700

    feat: 21 - merge two linked lists

---
+++ algorithms/python-algorithms/linked_list/21-merge_two_linked_lists.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# TC: O(n+m)
+#   - n = no of nodes in list1
+#   - m = no of nodes in list2
+# SC: O(1) - Constant space since only shifting pointers
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return dummy.next
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
